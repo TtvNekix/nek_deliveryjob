4bd1c4736649ce63e7a923acb4f85e0ec963e892
Juan <69590591+TtvNekix@users.noreply.github.com>
2021-09-21 13:40:54 -0700

    Delete nek_deliveryjob directory

--- nek_deliveryjob/Data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO `jobs` (name, label) VALUES
-  ('delivery', 'Delivery')
-;
-
-INSERT INTO `job_grades` (job_name, grade, name, label, salary, skin_male, skin_female) VALUES
-  ('delivery', 0, 'employee', 'Employee', 0, '{}', '{}')
-;
