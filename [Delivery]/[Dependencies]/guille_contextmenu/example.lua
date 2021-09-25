566fa7fd010f9043155d2e161b9589a6151d8902
Juan <69590591+TtvNekix@users.noreply.github.com>
2021-09-25 10:09:28 +0200

    Delete [Delivery] directory

--- [Delivery]/[Dependencies]/guille_contextmenu/example.lua
+++
@@ -1,8 +0,0 @@
-RegisterCommand("testmenu", function()
-    local data = {}
-    table.insert(data, {text = "Juanjo", toDo = [[TriggerServerEvent("hola")]]})
-    table.insert(data, {text = "Juanjo", toDo = [[TriggerServerEvent("hola1")]]})
-    table.insert(data, {text = "Juanjo", toDo = [[TriggerServerEvent("hola2")]]})
-    table.insert(data, {text = "Juanjo", toDo = [[TriggerServerEvent("hola3")]]})
-    TriggerEvent("guille_cont:client:open", "Hola", data)
-end)
